60c42058e0c54b02d38b02ab359c9f1a1dd9a92c
Petr Kurapov <petr.a.kurapov@intel.com>
2021-07-22 20:42:34 +0300

    Mark edges

--- graph_utils.py
+++ graph_utils.py
@@ -25,41 +25,51 @@ def get_het_graph_for(op: relops.Operator) -> nx.DiGraph:
     :param op:
     :return:
     """
+    cpu = CPU()
+    gpu = GPU()
     g = nx.DiGraph()
-    cpu_op = PlanNode(op, CPU())
-    gpu_op = PlanNode(op, GPU())
-    router = PlanNode(relops.Router(), CPU())
-    dummy = PlanNode(relops.Dummy(), CPU())
-    mm1 = PlanNode(relops.MemCpy(), CPU())
-    mm2 = PlanNode(relops.MemCpy(), CPU())
-    cpu2gpu = PlanNode(relops.CPU2GPU(), CPU())
-    gpu2cpu = PlanNode(relops.GPU2CPU(), CPU())
+    cpu_op = PlanNode(op, cpu)
+    gpu_op = PlanNode(op, gpu)
+    router = PlanNode(relops.Router(), cpu)
+    dummy = PlanNode(relops.Dummy(), cpu)
+    mm1 = PlanNode(relops.MemCpy(), cpu)
+    mm2 = PlanNode(relops.MemCpy(), gpu)
+    cpu2gpu = PlanNode(relops.CPU2GPU(), gpu)
+    gpu2cpu = PlanNode(relops.GPU2CPU(), gpu)
 
+    cpu_d = {"device": cpu}
+    gpu_d = {"device": gpu}
     g.add_nodes_from([cpu_op, gpu_op, router, dummy, mm1, mm2, cpu2gpu, gpu2cpu])
-    g.add_edges_from([(router, mm1), (router, mm2), (mm1, cpu_op), (cpu_op, dummy), (mm2, cpu2gpu), (cpu2gpu, gpu_op),
-                      (gpu_op, gpu2cpu), (gpu2cpu, dummy)])
+    g.add_edges_from(
+        [(router, mm1, cpu_d), (router, mm2, gpu_d), (mm1, cpu_op, cpu_d), (cpu_op, dummy, cpu_d), (mm2, cpu2gpu, gpu_d),
+         (cpu2gpu, gpu_op, gpu_d),
+         (gpu_op, gpu2cpu, gpu_d), (gpu2cpu, dummy, gpu_d)])
     assert nx.is_directed_acyclic_graph(g)
     return g
 
 
 def get_het_graph_for_join(op: relops.RelJoin) -> nx.DiGraph:
     g = nx.DiGraph()
-    cpu_op = PlanNode(op, CPU())
-    gpu_op = PlanNode(op, GPU())
-    router_a = PlanNode(relops.Router(), CPU())
-    router_b = PlanNode(relops.Router(), CPU())
-    mms = [PlanNode(relops.MemCpy(), CPU()) for _ in range(4)]
-    cpu2gpu_a = PlanNode(relops.CPU2GPU(), CPU())
-    cpu2gpu_b = PlanNode(relops.CPU2GPU(), CPU())
-    gpu2cpu = PlanNode(relops.GPU2CPU(), CPU())
-    dummy = PlanNode(relops.Dummy(), CPU())
+    cpu = CPU()
+    gpu = GPU()
+    cpu_op = PlanNode(op, cpu)
+    gpu_op = PlanNode(op, gpu)
+    router_a = PlanNode(relops.Router(), cpu)
+    router_b = PlanNode(relops.Router(), cpu)
+    mms = [PlanNode(relops.MemCpy(), cpu) for _ in range(4)]
+    cpu2gpu_a = PlanNode(relops.CPU2GPU(), gpu)
+    cpu2gpu_b = PlanNode(relops.CPU2GPU(), gpu)
+    gpu2cpu = PlanNode(relops.GPU2CPU(), gpu)
+    dummy = PlanNode(relops.Dummy(), cpu)
 
     g.add_nodes_from([cpu_op, gpu_op, router_a, router_b, cpu2gpu_a, cpu2gpu_b, gpu2cpu, dummy])
     g.add_nodes_from(mms)
 
-    g.add_edges_from([(router_a, mms[0]), (router_a, mms[1]), (mms[0], cpu_op), (cpu_op, dummy), (mms[1], cpu2gpu_a),
-                      (cpu2gpu_a, gpu_op), (gpu_op, gpu2cpu), (gpu2cpu, dummy), (router_b, mms[2]), (router_b, mms[3]),
-                      (mms[3], cpu2gpu_b), (cpu2gpu_b, gpu_op), (mms[2], cpu_op)])
+    cpu_d = {"device": cpu}
+    gpu_d = {"device": gpu}
+    g.add_edges_from([(router_a, mms[0], cpu_d), (router_a, mms[1], gpu_d), (mms[0], cpu_op, cpu_d), (cpu_op, dummy, cpu_d), (mms[1], cpu2gpu_a, gpu_d),
+                      (cpu2gpu_a, gpu_op, gpu_d), (gpu_op, gpu2cpu, gpu_d), (gpu2cpu, dummy, gpu_d), (router_b, mms[2], cpu_d), (router_b, mms[3], gpu_d),
+                      (mms[3], cpu2gpu_b, gpu_d), (cpu2gpu_b, gpu_op, gpu_d), (mms[2], cpu_op, cpu_d)])
     assert nx.is_directed_acyclic_graph(g)
     return g
 
