422cbfa8d0eb7212a34378ba1cfd8bde748096d9
ND-Daniel <darya_nikolskaya@mail.ru>
2021-11-12 15:49:43 +0300

    plan parser

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-import networkx as nx
-import relops
-from device import Device
+import string
+import sys
+import plan_parser
 import graph_utils
 from plan import Plan
-from data_params import DataParams
+import argparse
 from optimizer import optimize, genetic_search
+import json
 
 
-if __name__ == '__main__':
-    a_data = DataParams(100)
-    b_data = DataParams(500)
-    scan_a = graph_utils.get_het_graph_for(relops.RelScan("A", a_data))
-    scan_b = graph_utils.get_het_graph_for(relops.RelScan("B", b_data))
-    join = graph_utils.get_het_graph_for(relops.RelJoin())
-    res = graph_utils.connect_join(scan_a, scan_b, join)
-    final = graph_utils.finalize_graph_with(res, relops.RelSort())
-    # graph_utils.display(final)
+def main(args):
+    data = json.load(args.plan_input)
+    final = plan_parser.get_plan(data)
+    graph_utils.display(final)
+
     p = Plan(final)
 
     coeffs_len = len(p.get_features()['routers'])
@@ -24,7 +21,7 @@ if __name__ == '__main__':
     # p.set_routers_use_gpu_only()
     # print('All gpu cost: ', p.cost())
     # p.display()
-    p.set_routers_coeffs([0.5]*coeffs_len)
+    p.set_routers_coeffs([0.5] * coeffs_len)
     print('Half: ', p.cost())
     #
     # optimize(p)
@@ -34,3 +31,10 @@ if __name__ == '__main__':
     #
     # genetic_search(p)
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--plan_input', help='Path to input json file.', type=argparse.FileType('r'),
+                        default=sys.stdin)
+    args = parser.parse_args()
+    main(args)
