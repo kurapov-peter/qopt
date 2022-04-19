187f85987a833f0d612c3608ed683b185bcc0d92
Petr Kurapov <petr.kurapov@gmail.com>
2022-04-19 23:16:26 +0300

    Add proper arguments

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
-import string
-import sys
-import plan_parser
-import graph_utils
-from plan import Plan
 import argparse
-from optimizer import optimize, genetic_search, brute_force_opt
 import json
+import sys
+from enum import Enum
 
+import graph_utils
+import plan_parser
+from optimizer import brute_force_opt, firefly, genetic_search, optimize
+from plan import Plan
+
+
+class OptimizationAlgorithm(Enum):
+    bruteforce = 'bruteforce'
+    genetic = 'genetic'
+    firefly = 'firefly'
+    default = 'default'
+
+    def __str__(self) -> str:
+        return self.value
+
+def run_optimization(algorithm, plan):
+    match algorithm:
+        case OptimizationAlgorithm.bruteforce:
+            return brute_force_opt(plan)
+        case OptimizationAlgorithm.genetic:
+            return genetic_search(plan)
+        case OptimizationAlgorithm.firefly:
+            return firefly(plan)
+        case OptimizationAlgorithm.default:
+            return optimize(plan)
+        case _:
+            raise AttributeError('Invalid algorithm.')
 
 def main(args):
     data = json.load(args.plan_input)
     final = plan_parser.get_plan(data)
-    graph_utils.display(final)
+
+    if args.display:
+        graph_utils.display(final)
 
     p = Plan(final)
-
-    coeffs_len = len(p.get_features()['routers'])
     p.set_routers_use_cpu_only()
     print('All cpu cost: ', p.cost())
-    # p.set_routers_use_gpu_only()
-    # print('All gpu cost: ', p.cost())
-    # p.display()
-    p.set_routers_coeffs([0.5] * coeffs_len)
-    print('Half: ', p.cost())
-    #
-    # optimize(p)
-    #
-    # p.set_routers_coeffs([1 for _ in range(coeffs_len)])
-    # # print(p.cost())
-    #
-    # genetic_search(p)
-    brute_force_opt(p)
+    
+    run_optimization(args.algorithm, p)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--plan_input', help='Path to input json file.', type=argparse.FileType('r'),
                         default=sys.stdin)
+    parser.add_argument('-a', "--algorithm", type=OptimizationAlgorithm, choices=list(OptimizationAlgorithm), help='Optimization algorithm to use')
+    parser.add_argument("--display", action='store_true')
     args = parser.parse_args()
     main(args)
